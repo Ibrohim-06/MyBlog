19fdf812266960310cb0024f6f37304724487a81
Ibrohim-06 <ibrohim.sadriddinov06@gmail.com>
2026-07-21 08:20:23 +0500

    initial commit

---
+++ Backend/app.js
@@ -0,0 +1,14 @@
+import dotenv from "dotenv"
+dotenv.config()
+import express from "express"
+
+//variables
+const app = express()
+const port = process.env.PORT
+
+//middlewares
+
+//routes
+
+
+app.listen(port)
